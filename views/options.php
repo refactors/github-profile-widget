7da8aadfce372e51edc0ab89be28628a7f3d9841
Henrique Dias <hacdias@gmail.com>
2015-06-30 14:19:08 +0100

    all options added. fix #9

--- views/options.php
+++ views/options.php
@@ -14,7 +14,7 @@
 	       id="<?php echo $this->get_field_id( 'username' ); ?>"
 	       name="<?php echo $this->get_field_name( 'username' ); ?>"
 	       type="text"
-	       style="width: 70%"
+	       style="width: 90%"
 	       placeholder="Your GitHub username" max=""
 	       value="<?php echo $username ?>"/>
 	<?php if ( ! empty( $username ) ) { ?>
@@ -24,9 +24,9 @@
 	<?php } ?>
 </p>
 
-<p>Show:</p>
+<h4>Show:</h4>
 
-<div style="margin-left: 8px; margin-bottom: 8px;">
+<p>
 	<?php foreach ( $this->options["checkboxes"] as $option => $title ): ?>
 		<input class="checkbox" type="checkbox"
 			<?php checked( ${$option}, 'on' ); ?>
@@ -35,26 +35,50 @@
 		<label for="<?php echo $this->get_field_id( $option ) ?>">
 			<?php echo ucfirst( $title ) ?>
 		</label>
-		<br/>
+		<br>
 	<?php endforeach; ?>
-</div>
+</p>
 
-
-<hr>
+<h4>Advanced Settings</h4>
 
 <p>
-	<label for="<?php echo $this->get_field_id( 'oAuth' ); ?>"/>
+	<input class="checkbox" type="checkbox"
+		<?php checked( $cache, 'on' ); ?>
+		   id="<?php echo $this->get_field_id( 'cache' ) ?>"
+		   name="<?php echo $this->get_field_name( 'cache' ) ?>"/>
+	<label for="<?php echo $this->get_field_id( 'cache' ) ?>">
+		<?php echo ucfirst( 'Cache' ) ?>
+	</label>
+</p>
+
+<p style="font-size: 80%; color: gray;">
+	Cache is automatically enabled. If disabled the plugin may stop working after a while because there is an hourly
+	limit of calls to the GitHub's API.
+</p>
+
+<p>
+	<label for="<?php echo $this->get_field_id( 'cache_time' ); ?>"/>
+	<input
+		class="widefat"
+		id="<?php echo $this->get_field_id( 'cache_time' ); ?>"
+		name="<?php echo $this->get_field_name( 'cache_time' ); ?>"
+		type="number"
+		placeholder="Cache Expiration Time in Seconds"
+		value="<?php echo $cache_time ?>"/>
+</p>
+
+<p>
+	<label for="<?php echo $this->get_field_id( 'token' ); ?>"/>
 	<input class="widefat"
-	       id="<?php echo $this->get_field_id( 'oAuth' ); ?>"
-	       name="<?php echo $this->get_field_name( 'oAuth' ); ?>"
+	       id="<?php echo $this->get_field_id( 'token' ); ?>"
+	       name="<?php echo $this->get_field_name( 'token' ); ?>"
 	       type="text"
 	       placeholder="Your oAuth Token" max=""
-	       value="<?php echo $oAuth ?>"/>
+	       value="<?php echo $token ?>"/>
 </p>
-<div style="font-size: 80%; color: gray;">
+
+<p style="font-size: 80%; color: gray;">
 	<a href="https://github.com/settings/tokens/new" target="_blank">Create token</a>. If you don't set
 	it, the plugin will only be able to do 60 calls per hour to the GitHub API (which usually is not a problem, as the
 	plugin has cache).
-</div>
-
-<br/>
+</p>

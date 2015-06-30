ec575f0f44bc8448821914e7e6db96b3d81520aa
Luís Soares <lsoares@gmail.com>
2015-06-30 11:45:03 +0100

    clean

--- views/options.php
+++ views/options.php
@@ -1,38 +1,41 @@
 <p>
-	<label for="<?php echo $this->get_field_id( 'title' ); ?>"/>
-	<input class="widefat"
-	       id="<?php echo $this->get_field_id( 'title' ); ?>"
-	       name="<?php echo $this->get_field_name( 'title' ); ?>"
-	       type="text"
-	       placeholder="Title"
-	       value="<?php echo $title ?>"/>
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>"/>
+    <input class="widefat"
+           id="<?php echo $this->get_field_id( 'title' ); ?>"
+           name="<?php echo $this->get_field_name( 'title' ); ?>"
+           type="text"
+           placeholder="Title"
+           value="<?php echo $title ?>" />
 </p>
 
 <p>
-	<label for="<?php echo $this->get_field_id( 'username' ); ?>"/>
-	<input class="widefat"
-	       id="<?php echo $this->get_field_id( 'username' ); ?>"
-	       name="<?php echo $this->get_field_name( 'username' ); ?>"
-	       type="text"
-	       style="width: 70%"
-	       placeholder="Your GitHub username" max=""
-	       value="<?php echo $username ?>"/>
-	<?php if ( ! empty( $username ) ) { ?>
-		<a href='http://github.com/<?php echo $username ?>' target='_blank'>
-			<img src="<?php echo plugins_url( 'css/expand.png', dirname( __FILE__ ) ); ?>"/>
-		</a>
-	<?php } ?>
+    <label for="<?php echo $this->get_field_id( 'username' ); ?>"/>
+    <input class="widefat"
+           id="<?php echo $this->get_field_id( 'username' ); ?>"
+           name="<?php echo $this->get_field_name( 'username' ); ?>"
+           type="text"
+           style="width: 70%"
+           placeholder="Your GitHub username" max=""
+           value="<?php echo $username ?>"/>
+    <?php if ( ! empty( $username ) ) { ?>
+            <a href='http://github.com/<?php echo $username ?>' target='_blank'>
+                    <img src="<?php echo plugins_url( 'css/expand.png', dirname( __FILE__ ) ); ?>"/>
+            </a>
+    <?php } ?>
 </p>
 
 <p>
-	<label for="<?php echo $this->get_field_id( 'oAuth' ); ?>"/>
-	<input class="widefat"
-	       id="<?php echo $this->get_field_id( 'oAuth' ); ?>"
-	       name="<?php echo $this->get_field_name( 'oAuth' ); ?>"
-	       type="text"
-	       placeholder="Your oAuth Token" max=""
-	       value="<?php echo $oAuth ?>"/>
+    <label for="<?php echo $this->get_field_id( 'oAuth' ); ?>"/>
+    <input class="widefat"
+           id="<?php echo $this->get_field_id( 'oAuth' ); ?>"
+           name="<?php echo $this->get_field_name( 'oAuth' ); ?>"
+           type="text"
+           placeholder="Your oAuth Token" max=""
+           value="<?php echo $oAuth ?>"/>
 </p>
-<p style="font-size: 80%; color: gray;">
+<div style="font-size: 80%; color: gray;">
     <a href="https://github.com/settings/tokens/new" target="_blank">Create token</a>. If you don't set
-	it, the plugin will only be able to do 60 calls per hour to the GitHub API.</p>
+	it, the plugin will only be able to do 60 calls per hour to the GitHub API (which usually is not a problem, as the plugin has cache).
+</div>
+
+<br />

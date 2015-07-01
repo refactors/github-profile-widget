7baeeece15a47e71bae5b10e6dd2b9e754728453
Luís Soares <lsoares@gmail.com>
2015-07-01 22:29:09 +0100

    msg

--- plugin.php
+++ plugin.php
@@ -115,7 +115,7 @@ class GitHub_Profile extends WP_Widget {
 			$file = file_get_contents( $apiPath, false, $context );
 			if (!$file) {
 				echo 'Error with API; please provide ';
-				echo empty ( $config['token']) ? 'a token or increase cache time.' : 'another token.';
+				echo empty ( $config['token']) ? 'a token or increase cache time.' : 'a new token.';
 			}
 			update_option( $apiPath, $file );
 			update_option( $apiPath . 'time', microtime( true ) );
